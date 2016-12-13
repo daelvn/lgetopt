b2aa100c43eb44ac3672ab8699ae90b289b46a1c
Christopher Clements <bcnjr5@gmail.com>
2016-12-13 14:24:27 -0500

    Initial Commit

---
+++ getopt.lua
@@ -0,0 +1,187 @@
+-- Parses command-line arguments.
+
+-- On error, returns (nil, message, index).
+-- If the default "--help" handler is run, returns 'help'.
+
+return function(arg, opts)
+  local skip_opts = false
+
+  local result = {
+    opt = {},
+  }
+
+  local counter_cache = {}
+
+  local i = 0 -- start at 0 since loop increments first
+
+  while (i < #arg) do
+    i = i + 1
+
+    if (not (skip_opts)) and (#arg[i] > 0) then
+      if (arg[i] == '--') then
+        skip_opts = true
+        goto continue
+      end
+
+      if (opts.options and opts.options[arg[i]]) then
+        local name = arg[i]
+        local o = opts.options[arg[i]]
+
+        do
+          local value
+
+          if (o.type == 'string') then
+            i = i + 1
+            if (i > #arg) then
+              error('Option "'..name..'" requires a string', 2)
+            end
+            value = arg[i]
+          elseif (o.type == 'number') then
+            i = i + 1
+            if (i > #arg) then
+              error('Option "'..name..'" requires a number', 2)
+            end
+            value = tonumber(arg[i])
+          elseif (o.type == 'boolean') then
+            value = true
+          elseif (o.type == 'counter') then
+            if not (counter_cache[name]) then
+              counter_cache[name] = 1
+              value = 1
+            else
+              counter_cache[name] = counter_cache[name] + 1
+              value = counter_cache[name]
+            end
+          else
+            error('Invalid option type: '..tostring(o.type), 2)
+          end
+
+          if (o.callback) then
+            result.opt[name] = o.callback(value)
+          else
+            result.opt[name] = value
+          end
+        end
+
+        goto continue
+      end
+
+      if (arg[i]:sub(1,1) == '-') then
+        if (#arg[i] > 1) and (arg[i]:sub(2,2) == '-') then
+          -- only reached if "--help" is not defined
+          if (arg[i] == '--help') then -- default "--help" {{{
+            local format = require('string').format
+            local output = opts.help_output or require('io').write
+
+            if (opts.help_text) then
+              output(opts.help_text)
+              output("\n")
+            end
+
+            output("OPTIONS:\n")
+
+            local longest_opt = 4
+            local o = {}
+
+            if (opts.flags) then
+              for k,v in pairs(opts.flags) do
+                o[#o + 1] = {
+                  name = '  -'..k,
+                  desc = v.help_text or '(no help text)',
+                }
+
+                if (v.type == 'counter') then
+                  o[#o].name = o[#o].name..' ...'
+                end
+
+                if (o[#o].name:len() > longest_opt) then
+                  longest_opt = o[#o].name:len()
+                end
+              end
+            end
+
+            if (opts.options) then
+              for k,v in pairs(opts.options) do
+                local e = {
+                  desc = v.help_text or '(no help text)',
+                }
+
+                if (v.type == 'string') then
+                  e.name = '  '..k..' <string>'
+                elseif (v.type == 'number') then
+                  e.name = '  '..k..' <number>'
+                elseif (v.type == 'boolean') then
+                  e.name = '  '..k
+                elseif (v.type == 'counter') then
+                  e.name = '  '..k..' ...'
+                else
+                  error('Invalid option type: '..tostring(o.type), 2)
+                end
+
+                if (e.name:len() > longest_opt) then
+                  longest_opt = e.name:len()
+                end
+
+                o[#o + 1] = e
+              end
+            end
+
+            local fmtstr
+            if (longest_opt > 20) then
+              fmtstr = '%-20s'
+            else
+              fmtstr = '%-'..tostring(longest_opt)..'s'
+            end
+
+            for k,opt in pairs(o) do
+              output(format(fmtstr, opt.name))
+              if (opt.name:len() > 20) then
+                output("\n                      ")
+              else
+                output('  ')
+              end
+              output(opt.desc)
+              output("\n")
+              o[k] = nil
+            end
+
+            return 'help'
+          end -- }}}
+        elseif (opts.flags) then
+          local s = arg[i]:sub(2)
+
+          for c = 1, #s, 1 do
+            local f = s:sub(c,c)
+
+            if (opts.flags[f]) then
+              if (opts.flags[f].type == 'counter') then
+                if not (result.opt[f]) then
+                  result.opt[f] = 1
+                else
+                  result.opt[f] = result.opt[f] + 1
+                end
+              else
+                result.opt[f] = true
+              end
+            else
+              return nil, 'invalid flag: '..f, i
+            end
+          end
+          goto continue
+        end
+      end
+    end
+
+    -- unknown argument if this is reached
+
+    if not (result.unhandled) then
+      result.unhandled = {}
+    end
+
+    result.unhandled[#result.unhandled + 1] = arg[i]
+
+    ::continue::
+  end
+
+  return result
+end
---
+++ test.lua
@@ -0,0 +1,113 @@
+-- Tests the getopt module.
+-- Mostly just prints different things depending on the arguments.
+
+assert(arg ~= nil, 'Lua interpreter does not support command-line arguments')
+
+local getopt = require('getopt')
+local write = require('io').write
+
+local opts = {}
+
+do -- process arguments
+  local args = getopt(arg, {
+    help_text = 'test.lua -- getopt test script',
+    flags = {
+      ['c'] = {
+        help_text = 'Make the fox a cat.',
+      },
+      ['v'] = {
+        help_text = 'Increase verbosityness.',
+        type = 'counter',
+      },
+    },
+    options = {
+      ['--breed'] = {
+        help_text = 'The type of dog.',
+        type = 'string',
+      },
+      ['--name'] = {
+        help_text = "The dog's name.",
+        type = 'string',
+        callback = function(name)
+          return name:upper()
+        end,
+      },
+      ['--version'] = {
+        help_text = 'Display version information.',
+        type = 'boolean',
+        callback = function()
+          write("getopt test script v42\n")
+          require('os').exit(0)
+        end,
+      },
+    },
+  })
+
+  if (args == 'help') then -- help argument recieved, help text printed, exit
+    require('os').exit(0)
+  end
+
+  -- display all option values
+  write("Options:\n")
+  for option_name,value in pairs(args.opt) do
+    write("\t")
+    write('"'..option_name..'"')
+    write(' = ')
+
+    if (type(value) == 'string') then
+      write('"'..value..'"')
+    else
+      write(tostring(value))
+    end
+
+    write("\n")
+  end
+
+  write("\n")
+
+  if (args.unhandled) then
+    write("Unhandled arguments:\n")
+    for k,value in ipairs(args.unhandled) do
+      write("\t\""..value.."\"\n")
+    end
+
+    write("\n")
+
+    -- exit with error (unknown arguments)
+    error('Unknown argument: '..args.unhandled[1], 0)
+  end
+
+  opts = args.opt
+end
+
+-- do whatever the options say
+
+local verbosity = opts['v'] or 0
+local dog_breed = opts['--breed'] or 'labradoodle'
+local is_cat = opts['c']
+
+write('The quick brown ')
+
+if (is_cat) then
+  write('cat')
+else
+  write('fox')
+end
+
+write(' jumped over the lazy ')
+
+if (verbosity >= 1) then
+  write(dog_breed)
+else
+  write('dog')
+end
+
+if (opts['--name']) then
+  write(' called '..opts['--name'])
+end
+
+if (verbosity >= 2) then
+  write(" by bending it's legs, then straightening them quickly")
+end
+
+write(".\n")
